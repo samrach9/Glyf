ee3d674d7c024138038bbcaeb9d35b455b998da0
samrach9 <rachapudisamhita@gmail.com>
2024-02-17 14:18:06 -0800

    change to output

--- backend/whisperapi.py
+++ backend/whisperapi.py
@@ -10,13 +10,7 @@ load_dotenv()
 
 model_id = 'whisper-1'
 
-#transcript = client.audio.transcriptions.create(
-#    model = model_id,
-#    file = media_file
-#    response_format='srt'
-#
-
-audio_file= open("/Users/samhitarachapudi/My Drive/SaveTube.io-My mother was beheaded in front of me_ a survivor recalls Indias violent partition-(480p).mp3", "rb")
+audio_file= open("output.wav", "rb")
 transcript = client.audio.translations.create(
   model="whisper-1", 
   file=audio_file,

83ee52becca38c2750030a0198120e6108c42cac
ArranKooner <125759795+ArranKooner@users.noreply.github.com>
2024-02-17 10:59:51 -0800

    Create openai.py
    moved file.

---
+++ backend/openai.py
@@ -0,0 +1,26 @@
+# This code is for v1 of the openai package: pypi.org/project/openai
+import backend.openai as openai
+import jsonify
+import json
+client = openai.OpenAI(api_key="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")
+#import readingFronty
+
+def processAI(x):
+    response = client.chat.completions.create(
+        model="gpt-4",
+        messages=[
+        {
+            "role": "user",
+            "content": f"Please summarize this story, highlighting aspects that relate to cultural history"
+        }
+        ],
+        temperature=1,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+
+    jsonified_response=json.loads(response.json())
+    return(jsonified_response["choices"][0]["message"]["content"])

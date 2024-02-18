d30d42f340865ec400bf9df5de171c4055e4daec
anikas2003 <anikas2003@gmail.com>
2024-02-17 20:17:01 -0800

    hey

--- backend/data_modification.py
+++ backend/data_modification.py
@@ -1,24 +1,24 @@
 import pandas as pd
 import csv
 from csv import writer
+from openai_test import story
+from openai_test import attributes
+
 """
 # This file contains functions to modify the DATABASE.CSV file
 # Possible modifications include: 
 # 1) Adding a new Story + all of the tags that go with it 
 # 2) Updating the like count of a story given it's Submission Number
 """
-def NewStory(story, new_row):     # the story should be a string and new_row is a list of strings with the values for the columns in order
-    new_row.insert(0, story) # Add story to beginning of database
-    new_row.append(0) # Add like count to end of database (default for new story is 0)
+def NewStory(new_row):     # new_row is a list of strings with the values for the columns in order
+    new_row.append(0)      # Add like count to end of database (default for new story is 0)
 
     # Open our existing CSV file in append mode and Create a file object for this file
     with open('backend/database.csv', 'a') as f_object:
         # Pass this file object to csv.writer() and get a writer object
         writer_object = writer(f_object)
-    
         # Pass the list as an argument into the writerow()
         writer_object.writerow(new_row)
-    
         # Close the file object
         f_object.close()
 
@@ -40,22 +40,20 @@ def UpdateLikeCount(row_number):
     #update like count
     data_ld.at[row_number, "Likes"] = current_like_count
 
-
     # update CSV file
     data_ld._set_value(row_number, "Likes", current_like_count)
     data_ld.to_csv("backend/database.csv", index=False)
 
-    # print new data with new cell
-    print(data_ld)
+    # TESTING: print new data with new cell
+    # print(data_ld)
 
 
 
 # Testing
-# story = "Jack and Jill went up the hill To fetch a pail of water; Jack fell down and broke his crown, And Jill came tumbling after."
-# new_data = [1777, "Hike", "Death", "The Netherlands"]
-    
-# NewStory(story, new_data)
+# new_data = ["Jack and Jill went up the hill To fetch a pail of water; Jack fell down and broke his crown, And Jill came tumbling after.", "JACK AND JILL FELL", 1777, "Hike", "Death", "The Netherlands"]
+# print(type(new_data))
+
+# NewStory(new_data)
 # UpdateLikeCount(0)
-
-
+    
 
